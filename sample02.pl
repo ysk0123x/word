4d111d8ca86585f868868a7c1575ac79bd63761a
Yusuke Taniguchi <yusuke@localhost.localdomain>
2013-08-31 19:50:56 +0900

    mkdir File_aft

--- sample02.pl
+++ sample02.pl
@@ -3,31 +3,28 @@
 use strict;
 use Juman;
 
-# my $str = "この文を形態素解析して下さい。";
-#
 #INPUT FILE
-open(SRC,"./File/sample.txt");
+open(SRC,"./File/Acid.txt");
 print "見出し\t原形\t品詞\n\n";
 
 while(<SRC>){
-chomp;
-my $str = $_;
-my $juman = Juman->new;
-my $result = $juman->analysis( $str );
-foreach my $node ( $result->mrph() ) {
-    my $midasi = $node->midasi();
-#    $midasi =~ s/^\\//;
-    my $hinsi = $node->hinsi();  
-    my $hinsi_id = $node->hinsi_id();
-	my $genkei = $node->genkei();
- #   my $yomi = $node->yomi() if ( 1 < $hinsi_id && $hinsi_id < 15 );
- #   print $midasi, "\t", $yomi, "\t", $hinsi, "\n";
-	print "$midasi\t$genkei\t$hinsi\n";
-}
-print "\n";
+	chomp;
+	my $str = $_;
+	my $juman = Juman->new;
+	my $result = $juman->analysis( $str );
+	foreach my $node ( $result->mrph() ) {
+    	my $midasi = $node->midasi();
+	    $midasi =~ s/^\\//;
+    	my $hinsi = $node->hinsi();  
+    	my $hinsi_id = $node->hinsi_id();
+		my $genkei = $node->genkei();
+	#   my $yomi = $node->yomi() if ( 1 < $hinsi_id && $hinsi_id < 15 );
+ 	#   print $midasi, "\t", $yomi, "\t", $hinsi, "\n";
+		print "$midasi\t$genkei\t$hinsi\n";
+	}
+
+	print "\n";
 
 }
 
-
-
-
+close(SRC);
